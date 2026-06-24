4c1fe7b1315c95874927a2b24d02d0c06ec16019
Henri Issoufou <henri.issoufou@gmail.com>
2026-06-24 21:36:49 +0000

    Add multiply function

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,9 @@
-const { add } = require("../src/math");
+const { add, multiply } = require("../src/math");
 
 test("2 + 3 = 5", () => {
     expect(add(2, 3)).toBe(5);
+});
+
+test("2 * 3 = 6", () => {
+    expect(multiply(2, 3)).toBe(6);
 });
